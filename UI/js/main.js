0ba0954ea1dc361a7fd2162edca71bfe695364a2
walimike <walimike139@gmail.com>
2018-09-14 18:47:25 +0300

    creates add to cart functionality for user

--- UI/js/main.js
+++ UI/js/main.js
@@ -7,3 +7,22 @@ function SelectUser(){
   else if (document.getElementById('radiouser').checked){
 								window.location.href ="userhome.html";}
 }
+
+// function for adding items to Cart
+
+function addToCart() {
+    // clear selection table first
+    var selection = document.getElementsByTagName("table")[1].getElementsByTagName("tbody")[0];
+    selection.innerHTML = "";
+
+    // loop through items
+    var items = document.getElementsByTagName("tr");
+    for (var i = 0; i < items.length; i++) {
+      var input = items[i].getElementsByTagName("input");
+      if (!input.length) continue;
+      if (!input[0].checked) continue;
+      var item =  items[i].getElementsByTagName("td")[0].innerHTML;
+      // append element to selection
+      selection.innerHTML = selection.innerHTML + "<p>" + item;
+    }
+  }
