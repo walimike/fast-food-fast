a80d65e6289c5785a172105aa1e2ea24f7ff032c
walimike <walimike139@gmail.com>
2018-09-20 15:51:44 +0300

    fixes order status to models [#160618577]

--- app/models.py
+++ app/models.py
@@ -8,15 +8,16 @@ class OrderList(object):
     def __init__(self):
         pass
 
-    def add_order(self, order):
+    def add_order(self, order,price):
         self.id = self.id_manager(OrderList.order_list)
         self.order = order
         self.price = price
-        new_order = self.asdict(self.id,self.order,self.price)
+        self.status = "No"
+        new_order = self.asdict(self.id,self.order,self.price,self.status)
         OrderList.order_list.append(new_order)
 
-    def asdict(self, id, order, price):
-        return {'orderid':id,'order':order,'price':price}
+    def asdict(self, id, order, price,status):
+        return {'orderid':id,'order':order,'price':price, 'completed_status':status}
 
     def id_manager(self,list):
         if len(list)==0:
@@ -32,4 +33,6 @@ class OrderList(object):
         pass
 
     def retrieve_order(self,id):
-        pass
+        order = [order for order in OrderList.order_list if order['orderid'] == id]
+        if len(order)==1:
+            return order[0]["price"]
