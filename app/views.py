7170ed499ec89062a81df36f7961c015a30daa0c
Brian Serumaga <bsk20111@hotmail.com>
2018-09-27 14:25:28 +0300

    fixes routes for endpoints and adds error handler routes

--- app/views.py
+++ app/views.py
@@ -8,15 +8,15 @@ app = create_app(config_name='development')
 #welcome message for user.
 @app.route('/')
 def index():
-    return "<h1> Welcome to Fast Food Fast"
+    return "<h1> Welcome to Fast Food Fast </h1>"
 
 #fetches all orders, returns empty list, if none
-@app.route('/fastfoodfast/v1/orders', methods=['GET'])
+@app.route('/v1/orders', methods=['GET'])
 def get_all_orders():
     return jsonify({'orderlist':OrderList().order_list})
 
 # fetches a specific order
-@app.route('/fastfoodfast/v1/orders/<int:order_id>', methods=['GET'])
+@app.route('/v1/orders/<int:order_id>', methods=['GET'])
 def get_specific_order(order_id):
     specific_order = OrderList().retrieve_order(order_id)
     if not specific_order:
@@ -24,7 +24,7 @@ def get_specific_order(order_id):
     return jsonify({'food_item': specific_order})
 
 # endpoint for making an order
-@app.route('/fastfoodfast/v1/orders', methods=['POST'])
+@app.route('/v1/orders', methods=['POST'])
 def make_order():
     """checks whether request is in right format"""
     if not request.json or not 'order' in request.json:
@@ -34,24 +34,31 @@ def make_order():
     """checks whether order is in the food catalogue"""
     if not check_catalogue(recieved_order):
         abort(503)
+
+    """Checks if order is repeated"""
+    if not len(OrderList.order_list) == 0 and OrderList.order_list[-1]["order"] == recieved_order:
+        abort(403)
+          
     OrderList().add_order(recieved_order,check_catalogue(recieved_order))
     return jsonify({'orderlist': OrderList.order_list}), 201
 
 
 # endpoint for updating the completion status for an order
-@app.route('/fastfoodfast/v1/orders/<int:order_id>', methods=['PUT'])
+@app.route('/v1/orders/<int:order_id>', methods=['PUT'])
 def update_order(order_id):
     if not type(order_id)==int:
         abort(400)
-    if order_id<1 or order_id>(OrderList().id_limit()):
-        abort(404)
-    if not request.json and not (request.json()['completed_status']).isalpha():
-        abort(400)
+    if not request.json or not 'completed_status' in request.json:
+        abort(400)      
     new_status =  request.get_json()['completed_status']
-    if new_status.lower() != ("yes" or "no"):
-        abort(406)
+    if order_id > OrderList().id_limit():
+        return jsonify({"error":"ID out of range"})
+    if new_status.lower() != "yes":
+        if new_status.lower() != "no":
+            abort(406)
     OrderList().update_order(order_id,new_status)
     return jsonify({'orderlist':OrderList.order_list})
+    
 
  # error handlers
 @app.errorhandler(404)
@@ -59,14 +66,19 @@ def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
 
 @app.errorhandler(400)
-def not_found(error):
+def bad_request(error):
     return make_response(jsonify( { 'error': 'Bad request' } ), 400)
 
 @app.errorhandler(406)
-def not_found(error):
+def wrong_format(error):
     return make_response(jsonify( { 'error': 'This only takes a "Yes/No" response' } ), 406)
 
 
+@app.errorhandler(403)
+def repeat_order(error):
+    return make_response(jsonify( { 'error': 'You seem to be repeating the order' } ), 403)
+
 @app.errorhandler(503)
-def not_found(error):
-    return make_response(jsonify( { 'error': 'You seem to be repeating the order' } ), 503)
+def no_order(error):
+    return make_response(jsonify( { 'error': 'Order not currently in order list' } ), 503) 
+ 
