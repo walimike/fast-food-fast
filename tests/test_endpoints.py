b31da4adb914ed629b2cc4ff78e778746b038b46
Brian Serumaga <bsk20111@hotmail.com>
2018-09-27 19:05:42 +0300

    adds docstring comments to python files

--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -6,7 +6,9 @@ import unittest
 
 
 class EndpointsTestCase(unittest.TestCase):
-
+    """
+    Tests our API endpoints, run using pytest.
+    """
     def setUp(self):
         self.client = app.test_client()
         self.test_order = {"order":"matooke"}
@@ -17,35 +19,42 @@ class EndpointsTestCase(unittest.TestCase):
     def tearDown(self):
         OrderList.order_list = []    
 
-    # test for endpoints. Run using $pytest
     def test_can_get_all_orders(self):
+        """Tests whether endpoint can get all orders."""
         response = self.client.get('/v1/orders')
         self.assertEqual(response.status_code, 200)
 
     def test_can_make_an_order(self):
+        """Tests whether user can post an order."""
         res = self.client.post('/v1/orders', json = self.test_order)
         self.assertEqual(res.status_code, 201) 
 
     def test_can_not_repeat_order(self):
+        """Checks original length of orderlist is zero."""
         self.assertEqual(len(OrderList.order_list),0)     
         res = self.client.post('/v1/orders', json = self.test_order)
         self.assertEqual(res.status_code, 201)
+        """Repeats test_order subsequently."""
         res2 = self.client.post('/v1/orders', json = self.test_order)
+        """Checks that length is still 1 even after posting same order twice."""
         self.assertEqual(len(OrderList.order_list),1)
         res3 = self.client.post('/v1/orders', json = self.test_order2)
         self.assertEqual(len(OrderList.order_list),2)
 
     def test_can_get_specific_order(self):
+        """Tests whether endpoint can return one order based on ID."""
         response = self.client.get('/v1/orders/1')
         self.assertEqual(response.status_code, 404)
         self.client.post('/v1/orders', json = self.test_order , content_type='application/json')
         self.assertEqual(len(json.loads(response.data)), 1)
 
     def test_can_not_add_unknown_food(self):
+        """Tests that user can not make an order for commodity not on catalogue"""
         res = self.client.post('/v1/orders', json = self.test_order3)
         self.assertEqual(res.status_code, 503)
 
     def test_for_id_out_of_range(self):
+        """Tests that app raises error for ID out of range"""
         res = self.client.post('/v1/orders', json = self.test_order)
         res2 = self.client.post('/v1/orders', json = self.test_order2)
         response = self.client.get('/v1/orders/1')
@@ -54,11 +63,13 @@ class EndpointsTestCase(unittest.TestCase):
         self.assertEqual(response2.status_code, 404)
 
     def test_can_update_status(self):
+        """Tets whether status code for updating is 200"""
         res = self.client.post('/v1/orders', json = self.test_order)   
         response = self.client.put('/v1/orders/1', json = self.test_status1)
         self.assertEqual(response.status_code, 200)
         
     def test_can_change_status_update(self):
+        """Tests that the status changes from yes to know"""
         res = self.client.post('/v1/orders', json = self.test_order)
         order_status = OrderList.order_list[0]["completed_status"]
         self.assertEqual(order_status,"No") 
@@ -72,8 +83,10 @@ class EndpointsTestCase(unittest.TestCase):
         response2 = self.client.get('/v1/orders/-1')     
         self.assertEqual(response.status_code,404)
 
-    def test_must_have_right_ketword(self):
+    def test_must_have_right_keyword(self):
         """Tests whether json data has right key word"""
         data = {"ordr":"rice"}    
         res = self.client.post('/v1/orders', json = data)
         self.assertEqual(res.status_code,400)
+
+
--- tests/test_orderlist.py
+++ tests/test_orderlist.py
@@ -2,7 +2,9 @@ import unittest
 from app.models import OrderList
 
 class TestOrderList(unittest.TestCase):
-
+    """
+    Tests internal operations of our model class.
+    """
     def setUp1(self):
         OrderList().add_order('rice',5000)
 
@@ -14,12 +16,14 @@ class TestOrderList(unittest.TestCase):
         OrderList.order_list = []
 
     def test_can_add_order(self):
+        """Tests whether item can be added"""
         self.assertEqual(len(OrderList.order_list), 0)
         self.setUp1()
         self.assertEqual(len(OrderList.order_list), 1)
         self.tearDown()
 
     def test_can_generate_unique_id(self):
+        """Tests that unique IDs are generated foreach order"""
         self.setUp1()
         self.assertEqual(OrderList.order_list[-1]["orderid"],1)
         self.setUp2()
@@ -27,12 +31,15 @@ class TestOrderList(unittest.TestCase):
         self.tearDown()
 
     def test_can_update(self):
+        """Tests whether model can update"""
         self.setUp2()
         OrderList().update_order(2,"yes")
         self.assertEqual(OrderList.order_list[1]['completed_status'],"Yes")
         self.tearDown()  
 
     def test_can_retrieve_order(self):
+        """Tests whether order can be retrieved"""
         self.setUp2()
         order = OrderList().retrieve_order(2)
-        self.assertEqual(order["order"],"maize")      
+        self.assertEqual(order["order"],"maize")     
+        self.tearDown() 
