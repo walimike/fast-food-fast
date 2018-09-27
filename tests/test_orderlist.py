77007997f8f0252c6c51adf51e2bb551c6ddc1c3
Brian Serumaga <bsk20111@hotmail.com>
2018-09-27 17:43:24 +0300

    adds more tests for apiendpoints

--- tests/test_orderlist.py
+++ tests/test_orderlist.py
@@ -14,22 +14,25 @@ class TestOrderList(unittest.TestCase):
         OrderList.order_list = []
 
     def test_can_add_order(self):
-        self.assertEquals(len(OrderList.order_list), 0)
+        self.assertEqual(len(OrderList.order_list), 0)
         self.setUp1()
-        self.assertEquals(len(OrderList.order_list), 1)
+        self.assertEqual(len(OrderList.order_list), 1)
         self.tearDown()
 
     def test_can_generate_unique_id(self):
         self.setUp1()
-        self.assertEquals(OrderList.order_list[-1]["orderid"],1)
+        self.assertEqual(OrderList.order_list[-1]["orderid"],1)
         self.setUp2()
-        self.assertEquals(OrderList.order_list[-1]["orderid"],3)
+        self.assertEqual(OrderList.order_list[-1]["orderid"],3)
         self.tearDown()
-
-
 
     def test_can_update(self):
         self.setUp2()
         OrderList().update_order(2,"yes")
-        self.assertEquals(OrderList.order_list[1]['completed_status'],"Yes")
-        self.tearDown()    
+        self.assertEqual(OrderList.order_list[1]['completed_status'],"Yes")
+        self.tearDown()  
+
+    def test_can_retrieve_order(self):
+        self.setUp2()
+        order = OrderList().retrieve_order(2)
+        self.assertEqual(order["order"],"maize")      
